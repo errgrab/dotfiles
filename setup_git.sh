4b23c2cc98a060767dc69a6f5a839daae5182be3
ErikG <eriklustosa@gmail.com>
2025-01-25 20:51:46 +0000

    adding automation script

---
+++ setup_git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global user.email eriklustosa@gmail.com
+git config --global user.name ErikG
+git config --global user.signingkey $HOME/.ssh/id_rsa.pub
+git config --global gpg.format ssh
+git config --global commit.gpgsign true
+git config --global tag.gpgsign true
