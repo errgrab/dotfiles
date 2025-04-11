558e88f178ec08d90c61cbc2430f985e900b1f57
ErikG <eriklustosa@gmail.com>
2025-04-11 05:56:59 +0100

    update setup_git to not sign with ssh
    It is needed to be configured each time, and each key will become its own problem, I am accumulating ssh keys on github and I don't want that.

--- setup_git.sh
+++ setup_git.sh
@@ -2,7 +2,8 @@
 
 git config --global user.email eriklustosa@gmail.com
 git config --global user.name ErikG
-git config --global user.signingkey $HOME/.ssh/id_rsa.pub
-git config --global gpg.format ssh
-git config --global commit.gpgsign true
-git config --global tag.gpgsign true
+
+# git config --global user.signingkey $HOME/.ssh/id_rsa.pub
+# git config --global gpg.format ssh
+# git config --global commit.gpgsign true
+# git config --global tag.gpgsign true
