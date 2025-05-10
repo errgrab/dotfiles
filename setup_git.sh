c709ad953ba2d349a6738c31598436d5f12ee46e
ErikG <eriklustosa@gmail.com>
2025-05-10 22:12:26 +0000

    scripts that make my life easier (vibed)

--- setup_git.sh
+++ setup_git.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+set -eu
 
 git config --global user.email eriklustosa@gmail.com
 git config --global user.name ErikG
-
 # git config --global user.signingkey $HOME/.ssh/id_rsa.pub
 # git config --global gpg.format ssh
 # git config --global commit.gpgsign true
 # git config --global tag.gpgsign true
+git config --global --list
