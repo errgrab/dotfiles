27e38e498f725850a1f38955556a7940201c1ec3
ErikG <eriklustosa@gmail.com>
2025-05-30 00:09:01 +0100

    update

--- nixos/anon/config/tmux/tmux.nix
+++ nixos/anon/config/tmux/tmux.nix
@@ -2,5 +2,17 @@
 {
 	programs.tmux = {
 		enable = true;
+		baseIndex = 1;
+		customPaneNavigationAndResize = true;
+		disableConfirmationPrompt = true;
+		escapeTime = 1000;
+		extraConfig = builtins.readFile ./extra.conf;
+		historyLimit = 5000;
+		keyMode = "vi";
+		mouse = true;
+		newSession = true;
+		prefix = "`";
+		shell = "${pkgs.zsh}/bin/zsh";
+		terminal = "screen-256color";
 	};
 }
