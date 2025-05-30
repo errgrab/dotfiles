ff0dcbaf437fda35d30f1ccace2f77aa069f7fa9
ErikG <eriklustosa@gmail.com>
2025-05-30 12:19:27 +0100

    update

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,11 +3,20 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		home-manager.url = "github:nix-community/home-manager";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 	};
 
 	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-			modules = [ ./config.nix ];
+			modules = [
+				./config.nix
+				home-manager.nixosModules.home-manager {
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.anon = import ./anon/home.nix;
+				}
+			];
 		};
 	};
 }
