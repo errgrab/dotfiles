c8a08f328b94f74c6db5269d4567ecb69a446630
ErikG <eriklustosa@gmail.com>
2025-06-01 15:19:25 +0100

    update

--- nixos/anon/config/starship/starship.nix
+++ nixos/anon/config/starship/starship.nix
@@ -2,7 +2,7 @@
 	programs.starship = {
 		enable = true;
 		settings = {
-			prompt = "$status $all";
+			format = "$status $all";
 			add_newline = false;
 			line_break.disabled = true;
 			directory.truncation_length = 1;
