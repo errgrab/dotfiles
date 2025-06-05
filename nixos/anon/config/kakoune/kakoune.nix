b147f825f82b09d5fedeb69e4a25148bb45bfacf
ErikG <eriklustosa@gmail.com>
2025-06-06 00:40:55 +0100

    update

--- nixos/anon/config/kakoune/kakoune.nix
+++ nixos/anon/config/kakoune/kakoune.nix
@@ -12,7 +12,7 @@
 			};
 		};
 		extraConfig = ''
-addhl global/trl show-whitespaces -only-trailing -spc '·' -lf ' '
+addhl global/trl show-whitespaces -only-trailing -tab '»' -spc '·' -lf ' '
 addhl global/lcs show-whitespaces -tab '»' -spc ' ' -lf ' '
 '';
 	};
