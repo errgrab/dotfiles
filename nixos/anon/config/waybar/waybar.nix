146b28c36070aa8a260a4f001af7282bec954ca4
ErikG <eriklustosa@gmail.com>
2025-06-06 01:31:41 +0100

    update

--- nixos/anon/config/waybar/waybar.nix
+++ nixos/anon/config/waybar/waybar.nix
@@ -24,23 +24,23 @@
 					format-alt = "{:%d/%m/%Y}";
 				};
 				"memory" = {
-					format = "  {}%";
+					format = " {}%";
 				};
 				"cpu" = {
-					format = "  {usage}%";
+					format = " {usage}%";
 					tooltip = false;
 				};
 				"temperature" = {
 					critical-threshold = 80;
 					format = "{icon} {temperatureC}°C";
-					format-icons = [" " " " " "];
+					format-icons = ["" "" ""];
 				};
 				"network" = {
-					format-wifi = "  {essid} ({signalStrength}%)";
-					format-ethernet = " {ipaddr}/{cidr}";
-					tooltip-format = " {ifname} via {gwaddr}";
-					format-linked = " {ifname} (No IP)";
-					format-disconnected = "⚠ Disconnected";
+					format-wifi = "󰖩 {essid} ({signalStrength}%)";
+					format-ethernet = " {ipaddr}/{cidr}";
+					tooltip-format = "󰖩 {ifname} via {gwaddr}";
+					format-linked = "󰤠 {ifname} (No IP)";
+					format-disconnected = "󰤭 Disconnected";
 					format-alt = "{ifname} = {ipaddr}/{cidr}";
 				};
 				"pulseaudio" = {
