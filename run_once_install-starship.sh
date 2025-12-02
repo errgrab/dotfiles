08b138bf50fe1c9e24afe0bde4d547b9d4aec9ef
ErikG <eriklustosa@gmail.com>
2025-12-02 15:13:02 +0000

    remove starship

--- run_once_install-starship.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if ! command -v starship >/dev/null 2>&1; then
-	echo "Installing Starship..."
-	curl -sS https://starship.rs/install.sh | sh -s -- -y -b $HOME/.local/bin/
-else
-	echo "Starship is already installed."
-fi
