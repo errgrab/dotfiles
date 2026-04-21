adde69b26cfcb4037427ddd96afad5830c86b19d
ErikG <eriklustosa@gmail.com>
2026-04-21 11:55:04 +0100

    update

--- dot_config/emacs/custom.el
+++ dot_config/emacs/custom.el
@@ -4,9 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e900ae738225380abd1edc0c76535a12b8a6669c7a3180431ba0157a47bbf75e"
-     default)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -8,21 +8,52 @@
       initial-scratch-message nil
       initial-major-mode 'org-mode
       visible-bell t
-      reng-bell-fuunction 'ignore
-      catppuccin-flavor 'mocha
+      ring-bell-fuunction 'ignore
       custom-file "~/.config/emacs/custom.el")
+
+;; UTF-8 everywhere
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; Keep all backups in one place
+(let ((backup-dir "~/.config/emacs/backups/")
+      (autosave-dir "~/.config/emacs/autosaves/"))
+  (dolist (dir (list backup-dir autosave-dir))
+    (unless (file-directory-p dir)
+      (make-directory dir t)))
+  (setq backup-directory-alist         `((".*" . ,backup-dir))
+        auto-save-file-name-transforms `((".*" ,autosave-dir t))
+        auto-save-list-file-prefix     (concat autosave-dir ".saves-")))
+
+(setq backup-by-copying    t
+      delete-old-versions  t
+      kept-new-versions    6
+      kept-old-versions    2
+      version-control      t)
+
+;; Coding style
+(setq-default indent-tabs-mode nil
+	          tab-width 4
+	          c-basic-offset 4
+	          js-indent-level 2
+	          css-indent-offset 2)
+(setq electric-indent-inhibit nil)
+(electric-pair-mode 1)
+
+(add-hook 'c-mode-hook
+          (lambda () (c-set-style "k&r") (setq c-basic-offset 4)))
+(add-hook 'c++-mode-hook
+          (lambda () (c-set-style "k&r") (setq c-basic-offset 4)))
+(add-hook 'python-mode-hook
+          (lambda () (setq python-indent-offset 4)))
 
 ;; Enable some aditional things
 (column-number-mode t)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
 ;; Change font
-(set-face-attribute 'default nil :font "Adwaita Mono" :height 120)
-
-;; UTF-8 everywhere
-(set-language-environment "UTF-8")
-(set-default-coding-systems 'utf-8)
-(prefer-coding-system 'utf-8)
+(set-face-attribute 'default nil :font "Iosevka NF" :height 120)
 
 ;; Org-Mode with some customizations
 (require 'org)
@@ -37,14 +68,17 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Install catppuccin
-(use-package catppuccin-theme
-  :config (load-theme 'catppuccin :no-confirm))
+;; Setup theme
+(load-theme 'modus-vivendi)
 
 ;; Modeline more likable
 (use-package doom-modeline
   :init (doom-modeline-mode 1))
 
-;; VI-VI-VI
-;;(use-package evil
-;;  :config (evil-mode t))
+(cua-mode 1)
+(setq cua-keep-region-after-copy t)
+
+(use-package multiple-cursors
+  :bind (("M-n" . mc/mark-next-like-this)
+         ("M-p" . mc/mark-previous-like-this)
+         ("C-M-a" . mc/mark-all-like-this)))
